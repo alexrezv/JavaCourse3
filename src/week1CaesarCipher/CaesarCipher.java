4345c58a43b4609c7ba8a4a791b39e2947fb6c1c
AlexRezv <162809@niuitmo.ru>
2016-12-03 13:53:19 +0300

    Added code for two keys enc(dec)rypting

--- src/week1CaesarCipher/CaesarCipher.java
+++ src/week1CaesarCipher/CaesarCipher.java
@@ -6,14 +6,25 @@ package week1CaesarCipher;
 public class CaesarCipher {
     public static void main(String[] args) {
         String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int key = 15;
         System.out.println(abc);
         System.out.println(getRearrangedABC(abc, 19) + "\n");
 
-        String message = "HELLO MY DEAR FRIEND! HOW ARE YOU?" +
-                "\nHeloo me dea frend! Kak Dela?";
+        String message = "At noon be in the conference room with your hat on for a surprise party. YELL LOUD!";
         System.out.println(message);
-        System.out.println(getEncryptedMessage(abc, message, 19));
-        System.out.println(getDecryptedMessage(abc, getEncryptedMessage(abc, message, 19), 19));
+        String encmsg = getEncryptedMessage(abc, message, key);
+        System.out.println(encmsg);
+        String decmsg = getDecryptedMessage(abc, encmsg, key);
+        System.out.println(decmsg);
+
+        message = "First Legion";
+        System.out.println("\n" + message);
+        encmsg = getTwoEncryptedMessage(abc, message, 23, 17);
+        System.out.println(encmsg);
+        decmsg = getTwoDecryptedMessage(abc, encmsg, 23, 17);
+        System.out.println(decmsg);
+
+
 
     }
 
@@ -35,18 +46,44 @@ public class CaesarCipher {
         String rearrangedABC = getRearrangedABC(abc, key);
         String encmesg = "";
         for (int i = 0; i < message.length(); i++) {
+            encmesg = swapSymbol(abc, rearrangedABC, message, encmesg, i);
+        }
+        return encmesg;
+    }
 
-            if (abc.contains(Character.toUpperCase(message.charAt(i)) + "")) {
-                if (Character.isLowerCase(message.charAt(i))) {
-                    encmesg += Character.toLowerCase(
-                            rearrangedABC.charAt(abc.indexOf(Character.toUpperCase(message.charAt(i))))
-                    );
-                } else {
-                    encmesg += rearrangedABC.charAt(abc.indexOf(Character.toUpperCase(message.charAt(i))));
-                }
+    private static String getTwoEncryptedMessage(String abc, String message, int key1, int key2) {
+        return doTwoCaesarCipher(abc, message, key1, key2);
+    }
+
+    private static String getTwoDecryptedMessage(String abc, String message, int key1, int key2) {
+        return doTwoCaesarCipher(abc, message, Math.abs(26 - key1), Math.abs(26 - key2));
+    }
+
+    private static String doTwoCaesarCipher(String abc, String message, int key1, int key2) {
+        String rearrangedABC1 = getRearrangedABC(abc, key1);
+        String rearrangedABC2 = getRearrangedABC(abc, key2);
+        String encmesg = "";
+        for (int i = 0; i < message.length(); i++) {
+            if (i % 2 == 0) {
+                encmesg = swapSymbol(abc, rearrangedABC1, message, encmesg, i);
             } else {
-                encmesg += message.charAt(i);
+                encmesg = swapSymbol(abc, rearrangedABC2, message, encmesg, i);
             }
+        }
+        return encmesg;
+    }
+
+    private static String swapSymbol(String abc, String rearrABC, String message, String encmesg, int i) {
+        if (abc.contains(Character.toUpperCase(message.charAt(i)) + "")) {
+            if (Character.isLowerCase(message.charAt(i))) {
+                encmesg += Character.toLowerCase(
+                        rearrABC.charAt(abc.indexOf(Character.toUpperCase(message.charAt(i))))
+                );
+            } else {
+                encmesg += rearrABC.charAt(abc.indexOf(Character.toUpperCase(message.charAt(i))));
+            }
+        } else {
+            encmesg += message.charAt(i);
         }
         return encmesg;
     }
